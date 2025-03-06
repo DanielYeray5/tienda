eb7f5014526dddccc68e6eb9c275b55b45fa9184
DanielYeray5 <136346612+DanielYeray5@users.noreply.github.com>
2025-03-06 07:34:17 -0600

    third commit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const SaludarRoutes = require("./routes/greet.routes")
-const DespedirRoutes = require("./routes/despedir.routes")
+//! const SaludarRoutes = require("./routes/greet.routes")
+//! const DespedirRoutes = require("./routes/despedir.routes")
+
+const ProductRoutes = require('./routes/products.routes')
 const express = require('express')
 const cors = require('cors')
 const port = 3000;
@@ -14,6 +16,8 @@ app.use(cors())
 //app.use("/saludar",SaludarRoutes)
 //app.use("/despedir",DespedirRoutes)
 
+//? If is a API
+app.use('/api/v1', ProductRoutes)
 
 
 app.listen(port, () => console.log(`Server is running in http://localhost:${port}`))
